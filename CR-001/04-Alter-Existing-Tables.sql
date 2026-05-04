59ca6f0932eeda99d7f65d02d7e19947b3eef435
Mohammed Al Abri <striker.m901@gmail.com>
2026-05-04 14:39:58 +0400

    Update 04-Alter-Existing-Tables.sql

--- CR-001/04-Alter-Existing-Tables.sql
+++ CR-001/04-Alter-Existing-Tables.sql
@@ -2,36 +2,36 @@
 -- PART 4: Alter Existing Tables
 -- ============================================================
 
+USE SkyTrack
 -- ============================================================
--- 1. AIRPORTS
--- ============================================================
-
-
--- ============================================================
--- 2. AIRCRAFT
+-- FLIGHTS
 -- ============================================================
 
+--===== Add Airline Column =====--
+-- Step 1: Add column as nullable first to handle existing rows
+ALTER TABLE Flight ADD Airline VARCHAR(3) NULL;
 
--- ============================================================
--- 3. CREW
--- ============================================================
+-- Step 2: Update existing rows with a default airline
+UPDATE Flight SET Airline = 'EK';
 
+-- Step 3: Apply NOT NULL constraint now that all rows have a value
+ALTER TABLE Flight ALTER COLUMN Airline VARCHAR(3) NOT NULL;
 
--- ============================================================
--- 4. FLIGHTS
--- ============================================================
+-- Before Step 4 you need to update the Airline values to values in Flight to match what is in Airline table
+-- Step 4: Add FK to Airline
+ALTER TABLE Flight
+ADD CONSTRAINT FK_Flight_Airline
+    FOREIGN KEY (Airline) REFERENCES Airline(IATA)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE;
 
+--===== Add Gate code and airport =====--
+ALTER TABLE Flight
+ADD gate_code       VARCHAR(10) NULL,
+    gate_airport    VARCHAR(3)  NULL;
 
--- ============================================================
--- 5. FLIGHT CREW
--- ============================================================
-
-
--- ============================================================
--- 6. PASSENGERS
--- ============================================================
-
-
--- ============================================================
--- 7. BOOKINGS
--- ============================================================
+ALTER TABLE Flight
+ADD CONSTRAINT FK_Flight_Gate
+    FOREIGN KEY (gate_code, gate_airport) REFERENCES Gate(Code, Airport)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE;
